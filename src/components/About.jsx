58698dc827738d97c21c803aac657988f30beaf3
Icaro Apolo <apoloraci@gmail.com>
2023-03-12 20:32:21 -0300

    feat(about): add cards with animate and hoc section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import Tilt from 'react-tilt';
+import { motion } from 'framer-motion';
 
-const About = () => {
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc'
+
+const ServiceCard = ({ index, title, icon }) => {
   return (
-    <div>About</div>
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)} className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+        <div options={{ max: 45, scale: 1, speed: 450 }} className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center">
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
   )
 }
 
-export default About
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introdução</p>
+        <h2 className={styles.sectionHeadText}>Sobre mim</h2>
+      </motion.div>
+
+      <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+        Atualmente atuo como Desenvolvedor Front-End para uma empresa de tecnologia em minha cidade, sou responsável pela criação de websites e sistemas, trabalho com desenvolvimento de interfaces com a biblioteca React/Next.js, construção de aplicativos com React Native, consumo de dados via API para integrar aplicações e possuo contato direto com clientes em reviews por adotarmos a metodologia Scrum.
+        Também possuo conhecimentos em HTML, CSS, SASS, Styled Components, Bootstrap, Tailwind, Vite.js, Express, Laravel, MySQL, Firebase, Git e GitHub.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, "about")
