915e7da87b1513bc5329e0e604077a4b6c94c300
Icaro Apolo <apoloraci@gmail.com>
2023-03-13 14:47:48 -0300

    feat(header): change background when scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,22 @@ import { logo, menu, close } from '../assets';
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
+  const [navColor, setNavColor] = useState("transparent")
+
+  const listenScrollEvent = () => {
+    window.scrollY > 10 ? setNavColor("bg-primary shadow-md shadow-black") : setNavColor("transparent");
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenScrollEvent);
+    return () => {
+      window.removeEventListener("scroll", listenScrollEvent);
+    }
+  }, [])
 
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+    <nav className={`${styles.paddingX, navColor} w-full flex items-center py-5 fixed top-0 z-20`}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-5">
         <Link to="/" className="flex items-center gap-2" onClick={() => { setActive(""); window.scrollTo(0, 0); }}>
           <img src={logo} alt="Logo Portfolio" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">Icaro Apolo &nbsp;
