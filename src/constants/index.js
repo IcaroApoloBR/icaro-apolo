156ee40b81b9cd99f943dadc7a363381664883aa
Icaro Apolo <apoloraci@gmail.com>
2023-03-17 11:37:39 -0300

    feat(header): add section navbar

--- src/constants/index.js
+++ src/constants/index.js
@@ -35,6 +35,10 @@ export const navLinks = [
         title: "Work",
     },
     {
+        id: "projects",
+        title: "Projects",
+    },
+    {
         id: "contact",
         title: "Contact",
     },
