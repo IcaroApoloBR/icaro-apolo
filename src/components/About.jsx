2edca81a380fd880f4103d432fb5452ab2225880
Icaro Apolo <apoloraci@gmail.com>
2023-12-17 10:39:52 -0300

    fix: layout about cards from mobile screens

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ import Typed from 'typed.js'
 const ServiceCard = ({ index, title, icon }) => {
   return (
     <Tilt className="xs:w-52 w-full">
-      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)} className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)} className="contents xs:block w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
         <div options={{ max: 45, scale: 1, speed: 450 }} className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center">
           <img src={icon} alt={title} className="w-16 h-16 object-contain" />
           <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
